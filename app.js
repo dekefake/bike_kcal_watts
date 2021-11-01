1e1611138175b69afb449b90196dab4ec3c721f4
Thomas DE SA <thomasdesa@MacBook-Pro-de-Thomas.local>
2021-11-01 12:34:55 +0100

    10

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 $( document ).ready(function() {
     $('#calc').click(function() {
         var watts = $('#watts').val();
+        var minutes = $('#minutes').val();
+        var heures = Math.floor(minutes/60);
+        minutes = minutes - heures*60;
         var eff = 0.24;
         var calsec = 4.186;
-        var cal = watts / calsec / eff;
-        var ans = 'KCal pour 10 min : '+(Math.round(cal*0.6))+'<br>'+'KCal pour 1h : '+(Math.round(cal*3.6));
+        var kcalmin = watts / calsec / eff / 1000 * 60;
+        var time = minutes+' min';
+        if(heures>0){
+            time = heures+'h'+minutes+'min';
+            if(minutes==0) {
+                time = heures+'h';
+            }
+        }
+        var ans = 'KCal pour '+time+' : '+(Math.round(kcalmin*heures*60+kcalmin*minutes))+' KCal';
         $('.answer').html(ans);
     });
 });
